3115b798164cdc68189b9032aff0c09c2de15ea9
Zbiku <Zbiku@MSI>
2023-11-18 23:37:39 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int rozmiar;
+        Console.WriteLine("Podaj liczbę elementów tablicy: ");
+        rozmiar = int.Parse(Console.ReadLine());
+        int[] tablica = new int[rozmiar];
+        for (int i=0;i<tablica.Length;i++)
+        {
+            Console.WriteLine("Podaj liczbę: ");
+            tablica[i] = int.Parse(Console.ReadLine());
+        }
+        for (int i=0;i<tablica.Length;i++)
+        {
+            Console.Write(tablica[i] + " ");
+        }
+        Console.ReadKey();
+    }
+}
